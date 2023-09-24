3b6e786eae622c594414c22bb4d59727de447b70
Rhett Tien <46173055+RhettTien@users.noreply.github.com>
2023-09-25 01:50:57 +0800

    更新 downmp3.js

--- img/downmp3.js
+++ img/downmp3.js
@@ -1,9 +1,12 @@
 function processUrl(str) {
 
-    var extractedStr = str.split('=')[1].split('&')[0];
+    var extractedStr = str.match(/\d{3,}(?!\d{3,163})/); 
 
+    if (extractedStr == undefined || extractedStr == null || extractedStr == '') {
+        return "";
+    }else{
     var result = "https://music.163.com/song/media/outer/url?id=" + extractedStr + ".mp3";
-
+    }
     console.log(result);
     return result;  
 }
