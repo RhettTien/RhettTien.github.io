e25ea80cce89d699ee7d3d33ed555912e4b3156e
RhettTien <rhett.tien@aliyun.com>
2023-09-25 21:31:26 +0800

    update

--- img/downmp3.js
+++ img/downmp3.js
@@ -1,9 +1,14 @@
 function processUrl(str) {
 
-    var extractedStr = str.split('=')[1].split('&')[0];
+    var result = "";
+    const regex = /\d{4,}/; // 匹配4个或更多数字的正则表达式  
 
+    var extractedStr = str.match(regex);
+    if (extractedStr == "" || extractedStr == null || extractedStr == undefined) {
+        console.log("")
+        return "";
+    }
     var result = "https://music.163.com/song/media/outer/url?id=" + extractedStr + ".mp3";
-
-    console.log(result);
-    return result;  
+    console.log(result)
+    return result;
 }
